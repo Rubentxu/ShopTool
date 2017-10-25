07de4fa001d04bbff7ff916308595e1e1be36d7e
Rubentxu <rubentxu@rubentxu-ubuntu16.4>
2017-10-26 00:29:17 +0200

    microservicio pruebas producto funcionando con swagger

--- service.go
+++ service.go
@@ -1,66 +1,66 @@
-package pdigital
+package shoptool
 
 import (
 	"context"
 	"errors"
+	"log"
 	"time"
-)
 
-// Service for products
-type Service interface {
-	GetProduct(ctx context.Context, p Product) error
-}
+	"gopkg.in/mgo.v2/bson"
+)
 
 // Product type
 type Product struct {
-	IDSupplier             string       `json:"id_supplier"`
-	IDManufacturer         string       `json:"id_manufacturer"`
-	IDCategoryDefault      string       `json:"id_category_default"`
-	IDShopDefault          string       `json:"id_shop_default"`
-	OnSale                 bool         `json:"on_sale"`
-	OnlineOnly             bool         `json:"online_only"`
-	Ean13                  string       `json:"ean13"`
-	Isbn                   string       `json:"isbn"`
-	Upc                    string       `json:"upc"`
-	Ecotax                 float32      `json:"ecotax"`
-	Quantity               int32        `json:"quantity"`
-	MinimalQuantity        int32        `json:"minimal_quantity"`
-	Price                  float32      `json:"price"`
-	WholesalePrice         float32      `json:"wholesale_price"`
-	Unity                  string       `json:"unity"`
-	UnitPriceRation        float32      `json:"unit_price_ratio"`
-	AdditionalShippingCost float32      `json:"additional_shipping_cost"`
-	Reference              string       `json:"reference"`
-	SupplierReference      string       `json:"supplier_reference"`
-	Location               string       `json:"location"`
-	Width                  float32      `json:"width"`
-	Height                 float32      `json:"height"`
-	Depth                  float32      `json:"depth"`
-	Weight                 float32      `json:"weight"`
-	OutOfStock             int32        `json:"out_of_stock"`
-	QuantityDiscount       bool         `json:"quantity_discount"`
-	Customizable           int32        `json:"customizable"`
-	UploadableFiles        int32        `json:"uploadable_files"`
-	TextFields             int32        `json:"text_fields"`
-	Active                 int32        `json:"active"`
-	Redirect               RedirectType `json:"redirect_type"`
-	IDTypeRedirected       int32        `json:"id_type_redirected"`
-	AvailableForOrder      bool         `json:"available_for_order"`
-	AvailableDate          time.Time    `json:"available_date"`
-	ShowCondition          bool         `json:"show_condition"`
-	Condition              Condition    `json:"condition"`
-	ShowPrice              bool         `json:"show_price"`
-	Indexed                bool         `json:"indexed"`
-	Visibility             Visibility   `json:"visibility"`
-	CacheIsPack            bool         `json:"cache_is_pack"`
-	CacheHasAtachments     bool         `json:"cache_has_atachments"`
-	IsVirtual              bool         `json:"is_virtual"`
-	CacheDefaultAttribute  int32        `json:"cache_default_attribute"`
-	DateAdd                time.Time    `json:"date_add"`
-	DateUpd                time.Time    `json:"date_upd"`
-	AdvanceStockManagement bool         `json:"advanced_stock_management"`
-	PackStockType          bool         `json:"pack_stock_type"`
-	State                  bool         `json:"state"`
+	ID                     bson.ObjectId `json:"id"`
+	IDSupplier             bson.ObjectId `json:"id_supplier"`
+	IDManufacturer         bson.ObjectId `json:"id_manufacturer"`
+	IDCategoryDefault      bson.ObjectId `json:"id_category_default"`
+	IDShopDefault          bson.ObjectId `json:"id_shop_default"`
+	OnSale                 bool          `json:"on_sale"`
+	OnlineOnly             bool          `json:"online_only"`
+	Ean13                  string        `json:"ean13"`
+	Isbn                   string        `json:"isbn"`
+	Upc                    string        `json:"upc"`
+	Ecotax                 float32       `json:"ecotax"`
+	Quantity               int32         `json:"quantity"`
+	MinimalQuantity        int32         `json:"minimal_quantity"`
+	Price                  float32       `json:"price"`
+	WholesalePrice         float32       `json:"wholesale_price"`
+	Unity                  string        `json:"unity"`
+	UnitPriceRation        float32       `json:"unit_price_ratio"`
+	AdditionalShippingCost float32       `json:"additional_shipping_cost"`
+	Reference              string        `json:"reference"`
+	SupplierReference      string        `json:"supplier_reference"`
+	Location               string        `json:"location"`
+	Width                  float32       `json:"width"`
+	Height                 float32       `json:"height"`
+	Depth                  float32       `json:"depth"`
+	Weight                 float32       `json:"weight"`
+	OutOfStock             int32         `json:"out_of_stock"`
+	QuantityDiscount       bool          `json:"quantity_discount"`
+	Customizable           int32         `json:"customizable"`
+	UploadableFiles        int32         `json:"uploadable_files"`
+	TextFields             int32         `json:"text_fields"`
+	Active                 int32         `json:"active"`
+	Redirect               RedirectType  `json:"redirect_type"`
+	IDTypeRedirected       int32         `json:"id_type_redirected"`
+	AvailableForOrder      bool          `json:"available_for_order"`
+	AvailableDate          time.Time     `json:"available_date"`
+	ShowCondition          bool          `json:"show_condition"`
+	Condition              Condition     `json:"condition"`
+	ShowPrice              bool          `json:"show_price"`
+	Indexed                bool          `json:"indexed"`
+	Visibility             Visibility    `json:"visibility"`
+	CacheIsPack            bool          `json:"cache_is_pack"`
+	CacheHasAtachments     bool          `json:"cache_has_atachments"`
+	IsVirtual              bool          `json:"is_virtual"`
+	CacheDefaultAttribute  int32         `json:"cache_default_attribute"`
+	DateAdd                time.Time     `json:"date_add"`
+	DateUpd                time.Time     `json:"date_upd"`
+	AdvanceStockManagement bool          `json:"advanced_stock_management"`
+	PackStockType          bool          `json:"pack_stock_type"`
+	State                  bool          `json:"state"`
+	ProductsLang           []ProductLang `json:"productsLang"`
 }
 
 // ProductLang for products type
@@ -83,7 +83,7 @@ type RedirectType string
 const (
 	// None redirects
 	None RedirectType = "NONE"
-	// NotFound redirect
+	// NotFound redirect// Service for products
 	NotFound RedirectType = "404"
 	// MovedPermanentlyProduct redirect
 	MovedPermanentlyProduct RedirectType = "301PRODUCT"
@@ -122,3 +122,44 @@ var (
 	ErrAlreadyExists   = errors.New("already exists")
 	ErrNotFound        = errors.New("not found")
 )
+
+// ProductService for products
+type ProductService interface {
+	GetProduct(ctx context.Context, id string) (Product, error)
+	GetAllProducts(ctx context.Context) ([]Product, error)
+	DeleteProduct(ctx context.Context, id string) error
+	CreateProduct(ctx context.Context, p *Product) error
+}
+
+type productService struct {
+	repo *ProductRepository
+}
+
+// NewProductService create service products
+func NewProductService() (ProductService, error) {
+	var p ProductService
+	r, err := NewProductRepository()
+	if err != nil {
+		log.Fatalf("[Error Create ProductService]: %s\n", err)
+		return p, err
+	}
+	return &productService{
+		repo: &r,
+	}, nil
+}
+
+func (s *productService) GetProduct(ctx context.Context, id string) (Product, error) {
+	return s.repo.GetByID(id)
+}
+
+func (s *productService) GetAllProducts(ctx context.Context) ([]Product, error) {
+	return s.repo.GetAll(), nil
+}
+
+func (s *productService) DeleteProduct(ctx context.Context, id string) error {
+	return s.repo.Delete(id)
+}
+
+func (s *productService) CreateProduct(ctx context.Context, p *Product) error {
+	return s.repo.Create(p)
+}
